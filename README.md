52a820a34bb50e2dfd11d1b318232087276989f7
Oshani Seneviratne <oshanis@gmail.com>
2024-01-21 10:42:55 -0500

    Modularized the navbar

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# phkg.github.io
+# How to run the web server
+
+`python3 -m http.server`
+(You may also view the raw HTML files, but some content, such as the navbar, might not appear due to CORS policy.)
--- assets/js/main.js
+++ assets/js/main.js
@@ -228,4 +228,9 @@
     })
   });
 
+  document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+      .then(response => response.text())
+      .then(text => document.getElementById('navbar-placeholder').innerHTML = text);
+  });
 })()
